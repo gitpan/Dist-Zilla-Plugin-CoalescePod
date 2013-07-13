63470518e5acfd9db87268318deecad09a3cf1e0
Yanick Champoux <yanick+cpan@babyl.dyndns.org>
2013-07-13 18:56:29 +0000

    Import of YANICK/Dist-Zilla-Plugin-CoalescePod-0.2.0 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-CoalescePod
    gitpan-cpan-version:      0.2.0
    gitpan-cpan-path:         YANICK/Dist-Zilla-Plugin-CoalescePod-0.2.0.tar.gz
    gitpan-cpan-author:       YANICK
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/CoalescePod.pm
+++ lib/Dist/Zilla/Plugin/CoalescePod.pm
@@ -1,13 +1,14 @@
-use strict;
-use warnings;
 package Dist::Zilla::Plugin::CoalescePod;
 BEGIN {
   $Dist::Zilla::Plugin::CoalescePod::AUTHORITY = 'cpan:YANICK';
 }
 {
-  $Dist::Zilla::Plugin::CoalescePod::VERSION = '0.1.0';
+  $Dist::Zilla::Plugin::CoalescePod::VERSION = '0.2.0';
 }
 # ABSTRACT: merge .pod files into their .pm counterparts
+
+use strict;
+use warnings;
 
 use Moose;
 
@@ -16,32 +17,40 @@ with 'Dist::Zilla::Role::FileMunger';
 sub munge_file {
     my ( $self, $file ) = @_;
 
-    return unless $file->name =~ /\.pm$/;
+    # only look under /lib
+    return unless $file->name =~ m#^lib/.*\.pm$#;
 
     ( my $podname = $file->name ) =~ s/\.pm$/.pod/;
 
     my ( $podfile ) = grep { $_->name eq $podname } 
                            @{ $self->zilla->files } or return;
 
-    $file->content(
-        $file->content . $podfile->content
-    );
+   $self->log( "merged $podfile into " . $file->name );
+
+    my @content = split /(^__DATA__$)/m, $file->content;
+
+    # inject the pod
+    splice @content, 1, 0, $podfile->content;
+
+    $file->content( join '', @content );
 
     $self->zilla->prune_file($podfile);
 }
 
 1;
+#PODNAME: Foo
 
 __END__
+
 =pod
 
 =head1 NAME
 
-Dist::Zilla::Plugin::CoalescePod - merge .pod files into their .pm counterparts
+Foo - merge .pod files into their .pm counterparts
 
 =head1 VERSION
 
-version 0.1.0
+version 0.2.0
 
 =head1 SYNOPSIS
 
@@ -65,4 +74,3 @@ This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
